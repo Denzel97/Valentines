d85304e3b72b89bf26ba069ea3e531569da1c714
denzel97 <denzelwash97@gmail.com>
2024-02-13 21:47:35 +0300

    modified ALLOWEDHOST to *

--- valentine_project/settings.py
+++ valentine_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o)o#8uh$j_2j)huys0_igi2%gocm47-j!vfk8o+*ogr8@28&)+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
